6edee0d29c16f5c3f6f700d12788b13d06afd85b
Jiří Kofránek <kofranek@gmail.com>
2021-09-29 22:49:36 +0200

    first components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Counter from './components/Counter';
 
 function App () {
-
-  const [likes, setLikes] = useState(6)
-  const [value, setValue] = useState('text in input')
-
-  function increment () {
-    setLikes(likes + 1)
-  }
-
-  function decrement () {
-    setLikes(likes - 1)
-  }
-
   return (
     <div className="App">
-     <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
     </div>
   );
 }
