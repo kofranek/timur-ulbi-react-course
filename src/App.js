2b48fd4c987777c95936b62e958e5b31b73895f3
Jiří Kofránek <kofranek@gmail.com>
2021-09-30 00:02:25 +0200

    styly css classes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import Counter from './components/Counter';
-import ClassCounter from './components/ClassCounter';
+import Counter from './components/Counter'
+import ClassCounter from './components/ClassCounter'
+import './styles/App.css'
 
 function App () {
   return (
     <div className="App">
-      <ClassCounter/>
-      <ClassCounter/>
-      <ClassCounter/>
+
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javascript</strong>
+          <div>
+            Javascript - programovací jazyk
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Odstranit</button>
+        </div>
+      </div>
+
     </div>
   );
 }
